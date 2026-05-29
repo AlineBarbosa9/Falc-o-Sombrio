f687193860d7a45b97cbd5db2e27cc16da421e9b
Sofia Castelli <castelli.sofiaxyz@gmail.com>
2026-05-29 14:02:04 -0300

    Atualização da integração do banco de dados

--- Falcão_Sombrio/src/model/Telemetria.java
+++ Falcão_Sombrio/src/model/Telemetria.java
@@ -1,12 +1,14 @@
 package model;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public class Telemetria {
-	
-	// Atributos Privados
+
+    // Atributos Privados
     private final UUID id;
     private final UUID droneId;
+    private final UUID missaoId;
     private final double latitude;
     private final double longitude;
     private final double altitude;
@@ -14,14 +16,15 @@ public class Telemetria {
     private final Instant timestamp;
 
     // Construtor Público
-    public Telemetria(UUID droneId, double latitude, double longitude, double altitude,
-            double velocidade, String statusDrone, String origemDado, Instant timestamp){
-    	
+    public Telemetria(UUID droneId, UUID missaoId, double latitude, double longitude,
+            double altitude, double velocidade, Instant timestamp) {
+
         if (droneId == null) throw new IllegalArgumentException("DroneId não pode ser nulo");
         if (velocidade < 0) throw new IllegalArgumentException("Velocidade inválida");
 
         this.id = UUID.randomUUID();
         this.droneId = droneId;
+        this.missaoId = missaoId;
         this.latitude = latitude;
         this.longitude = longitude;
         this.altitude = altitude;
@@ -30,31 +33,34 @@ public class Telemetria {
     }
 
     // Getters
-	public UUID getId() {
-		return id;
-	}
-	public UUID getDroneId() {
-		return droneId;
-	}
-	public double getLatitude() {
-		return latitude;
-	}
-	public double getLongitude() {
-		return longitude;
-	}
-	public double getAltitude() {
-		return altitude;
-	}
-	public double getVelocidade() {
-		return velocidade;
-	}
-	public Instant getTimestamp() {
-		return timestamp;
-	}
-	
-	@Override
+    public UUID getId() {
+        return id;
+    }
+    public UUID getDroneId() {
+        return droneId;
+    }
+    public UUID getMissaoId() {
+        return missaoId;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public double getAltitude() {
+        return altitude;
+    }
+    public double getVelocidade() {
+        return velocidade;
+    }
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
     public String toString() {
-        return String.format("Telemetria[DroneID=%s, Lat=%.6f, Lon=%.6f, Alt=%.2f, Vel=%.2f, TS=%s]",
-                droneId, latitude, longitude, altitude, velocidade, timestamp);
+        return String.format("Telemetria[DroneID=%s, MissaoID=%s, Lat=%.6f, Lon=%.6f, Alt=%.2f, Vel=%.2f, TS=%s]",
+                droneId, missaoId, latitude, longitude, altitude, velocidade, timestamp);
     }
 }
